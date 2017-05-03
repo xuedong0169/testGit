3d87266573c6e3d4d0ceff6b7ab6a41c6fbfc40d
xuedong <xuedong0169@outlook.com>
2017-05-03 16:03:12 +0800

    testGit First

---
+++ testGit/src/cn/itcast/test/Test.java
@@ -0,0 +1,17 @@
+package cn.itcast.test;
+/**
+*@project  项目名称：testGit
+* @author 作者 lenovo  E-mail:
+* @version 创建时间：2017年5月3日 下午3:57:41
+* 类说明
+*/
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
